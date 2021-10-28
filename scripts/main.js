4ac85a31f6c8ca058de433f47999dbd89719bd4c
armoredmenace <91107932+armoredmenace@users.noreply.github.com>
2021-10-27 23:04:52 -0400

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
-function main()
-{
-    var canvas = document.getElementById("mainCanvas");
-    var ctx=canvas.getContext("2d");
+window.addEventListener('load', () =>{
+    const canvas = document.querySelector("#canvas");
+    const ctx=canvas.getContext("2d");
+
     ctx.fillStyle = "ffffff";
     ctx.fillRect(0,0,1260,400);
-}
+});
+
+window.addEventListener('resize', () => {
+    //Resizing
+    canvas.width=window.innerWidth;
+    canvas.height=1257;
+});
