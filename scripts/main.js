f880d7a9b6cd6c974f63ba47e3fd031ad58b6c62
armoredmenace <91107932+armoredmenace@users.noreply.github.com>
2021-10-27 23:06:34 -0400

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,11 @@ window.addEventListener('load', () =>{
     const canvas = document.querySelector("#canvas");
     const ctx=canvas.getContext("2d");
 
-    ctx.fillStyle = "ffffff";
-    ctx.fillRect(0,0,1260,400);
-});
-
-window.addEventListener('resize', () => {
     //Resizing
     canvas.width=window.innerWidth;
     canvas.height=1257;
+
+    //drawing main canvas for web browser
+    ctx.fillStyle = "ffffff";
+    ctx.fillRect(0,0,1260,400);
 });
